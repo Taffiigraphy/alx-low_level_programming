f5d7312ff2074570c490f48d57f0447696bc27af
Taffiigraphy <vt.maverudze@gmail.com>
2023-04-18 06:16:58 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that prints something
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
