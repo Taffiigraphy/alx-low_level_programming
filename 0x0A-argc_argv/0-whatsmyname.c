7121d7854db04688535f30db4bb80b81a0d3ebb0
Taffiigraphy <vt.maverudze@gmail.com>
2023-04-23 13:20:56 -0700

    commit one

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - int function
+ * @argc: num of args
+ * @argv: ary of args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
